8767ab861afe641085d47163737eedd717f6e348
Quema <myoon0929@gmail.com>
2023-08-04 22:52:19 +0900

    fin?

--- Quality.js
+++ Quality.js
@@ -47,10 +47,9 @@ let Youtube = () => {
                         spanElement.click();
                         break; // 2160p를 찾지 못한 경우 1440p를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다.
                     } else if (spanElement.textContent.trim() === '1080p HD' || spanElement.textContent.trim() === '1080p60 HD') {
-                        console.log(spanElement.textContent)
                         spanElement.click();
                         break; // 2160p와 1440p를 찾지 못한 경우 1080p를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다.
-                    } else if ((spanElement.textContent.trim() === '1080p Premium HDEnhanced bitrate') === (spanElement.textContent.trim() === '1080p HD' || spanElement.textContent.trim() === '1080p60 HD')) {
+                    } else if (spanElement.textContent.trim() === '1080p Premium HD') {
                         spanElement.click();
                         break; // 2160p와 1440p, 1080p를 찾지 못한 경우 1080p Premium를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다
                     } 
@@ -61,19 +60,18 @@ let Youtube = () => {
                 for (const span of spanTag) {
                     if (span.textContent.includes('2160p')) {
                         span.click();
-                       break; // 2160p를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다.
+                        break; // 2160p를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다.
                     } else if (span.textContent.includes('1440p')) {
                         span.click();
                         break; // 2160p를 찾지 못한 경우 1440p를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다.
-                    } else if (span.textContent.trim() === '1080p HD' || span.textContent.trim() === '1080p60 HD' ) {
+                    } else if (span.textContent.trim() === '1080p HD' || span.textContent.trim() === '1080p60 HD') {
                         span.click();
                         break; // 2160p와 1440p를 찾지 못한 경우 1080p를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다.
-                    } else if (span.textContent.trim() === '1080p Premium HDEnhanced bitrate') {
+                    } else if (span.textContent.trim() === '1080p Premium HD') {
                         span.click();
                         break; // 2160p와 1440p, 1080p를 찾지 못한 경우 1080p Premium를 찾았으면 더 이상 탐색하지 않고 반복문을 종료합니다
                     } else if (span.textContent.includes('Auto')) {
                         span.click();
-                        console.log("Auto");
                         break; 
                     }
                 }
