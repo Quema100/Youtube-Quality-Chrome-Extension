b62d475225ddd0ead75c1d9efed23e9410475d34
Quema <myoon0929@gmail.com>
2024-04-21 17:04:33 +0900

    add warning skip code

--- js/skip.js
+++ js/skip.js
@@ -1,16 +1,23 @@
 let skip=()=>{
-    if (window.location.pathname.startsWith('/watch')){
-        const skipButton = document.querySelector('.ytp-ad-skip-button');
-        if(skipButton != undefined){
-            console.log("skip");
-            skipButton.click();
-        };
-    };
+
     const skipButton = document.querySelector('.ytp-skip-ad-button');
     if(skipButton != undefined){
         console.log("skip")
         skipButton.click();
     }
+
+    const warningButton = document.querySelector('button[aria-label="절차를 이해했으며 계속 진행하기 원합니다."]');
+    if(warningButton != undefined){
+        console.log("skip")
+        warningButton.click();
+    }
+
+    const warningButtonEn = document.querySelector('button[aria-label="I understand and wish to proceed"]');
+    if(warningButtonEn != undefined){
+        console.log("skip")
+        warningButtonEn.click();
+    }
+
     const YesButton = document.querySelector('yt-button-renderer[id="confirm-button"] > yt-button-shape > button[aria-label="Yes"]')
     if (YesButton !== null){
         console.log("yes");
